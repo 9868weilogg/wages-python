b95ce7809397d5ece96802272e9b0af58e72e3ee
Wei Logg <william.wlcheah@gmail.com>
2020-05-17 23:59:06 +0800

    crawl stock list, crawl klse report

--- Router.py
+++ Router.py
@@ -26,51 +26,56 @@ def route(params):
 
 	### Called by API
 	Logger.v('route:',params);
-	if act[0] == 'test':
-		if act[1] == 'whitelist':
-			result = Report.get(act[1], params);
-	if act[0] == 'generate':
-		if act[1] == 'dropdownlist':
-			result = Filter.getDropdownList(params);
-		if act[1] == 'updatereport':
-			result = Crawl.updateReportData(params);
-		if act[1] == 'budget':
-			result = Report.get(act[1], params);
-		if act[1] == 'procurement':
-			result = Report.get(act[1], params);
-	if act[0] == 'get':
-		if act[1] == 'stock':
-			if act[2] == 'check':
-				result = Report.get('{0}{1}'.format(act[1], act[2]), params);
-			if act[2] == 'integrity':
-				result = ReportStockIntegrity.checkIntegrity(params);
-			if act[2] == 'backdate':
-				result = ReportStock.getBackdateList(params);
-		if act[1] == 'stock-issue':
-			if act[2] == 'integrity':
-				result = ReportStockIssueIntegrity.check(params);
-			if act[2] == 'options':
-				result = ReportStockIssue.getOption(params);
-			if act[2] == 'check':
-				result = ReportStockIssue.check(params);
-		if act[1] == 'facility':
-			result = ReportFacility.get(params);
 	if act[0] == 'crawl':
-		if act[1] == 'main':
-			result = Crawl.run(params);
-		if act[1] == 'start':
-			result = Crawl.start(params);
-		if act[1] == 'schedule':
-			result = Crawl.prepareSchedule(params);
-	if act[0] == 'upload':
-		if act[1] == 'stock':
-			result = ModelStock.upload(params);
-		if act[1] == 'facility':
-			result = ModelFacility.upload(params);
-		if act[1] == 'stock-issue':
-			result = ModelStockIssue.upload(params);
-	if act[0] == 'search':
-		if act[1] == 'stock': ## search_stock
-			result = ReportItem.find(params);
+		if act[1] == 'klsescreener':
+			result = Crawl.sourceKlsescreener(params);
+		if act[1] == 'bursamalaysia':
+			result = Crawl.sourceBursaMalaysia(params);
+	# if act[0] == 'test':
+	# 	if act[1] == 'whitelist':
+	# 		result = Report.get(act[1], params);
+	# if act[0] == 'generate':
+	# 	if act[1] == 'dropdownlist':
+	# 		result = Filter.getDropdownList(params);
+	# 	if act[1] == 'updatereport':
+	# 		result = Crawl.updateReportData(params);
+	# 	if act[1] == 'budget':
+	# 		result = Report.get(act[1], params);
+	# 	if act[1] == 'procurement':
+	# 		result = Report.get(act[1], params);
+	# if act[0] == 'get':
+	# 	if act[1] == 'stock':
+	# 		if act[2] == 'check':
+	# 			result = Report.get('{0}{1}'.format(act[1], act[2]), params);
+	# 		if act[2] == 'integrity':
+	# 			result = ReportStockIntegrity.checkIntegrity(params);
+	# 		if act[2] == 'backdate':
+	# 			result = ReportStock.getBackdateList(params);
+	# 	if act[1] == 'stock-issue':
+	# 		if act[2] == 'integrity':
+	# 			result = ReportStockIssueIntegrity.check(params);
+	# 		if act[2] == 'options':
+	# 			result = ReportStockIssue.getOption(params);
+	# 		if act[2] == 'check':
+	# 			result = ReportStockIssue.check(params);
+	# 	if act[1] == 'facility':
+	# 		result = ReportFacility.get(params);
+	# if act[0] == 'crawl':
+	# 	if act[1] == 'main':
+	# 		result = Crawl.run(params);
+	# 	if act[1] == 'start':
+	# 		result = Crawl.start(params);
+	# 	if act[1] == 'schedule':
+	# 		result = Crawl.prepareSchedule(params);
+	# if act[0] == 'upload':
+	# 	if act[1] == 'stock':
+	# 		result = ModelStock.upload(params);
+	# 	if act[1] == 'facility':
+	# 		result = ModelFacility.upload(params);
+	# 	if act[1] == 'stock-issue':
+	# 		result = ModelStockIssue.upload(params);
+	# if act[0] == 'search':
+	# 	if act[1] == 'stock': ## search_stock
+	# 		result = ReportItem.find(params);
 	# fn.show(result);
 	return result;
